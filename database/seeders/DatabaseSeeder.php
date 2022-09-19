843a7905a5e76253d7dfdf575d8426f67db1209f
Shankara21 <lazuardit21@gmail.com>
2022-09-19 13:26:15 +0700

    updating sub category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             RoleSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            SubCategorySeeder::class
         ]);
     }
 }
--- database/seeders/SubCategorySeeder.php
+++ database/seeders/SubCategorySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\SubCategory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SubCategorySeeder extends Seeder
 {
@@ -14,6 +17,128 @@ class SubCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = collect([
+            'Php',
+            'Java',
+            'JavaScript',
+            'Html',
+            'Laravel',
+            'Code Igniter',
+            'React Js',
+            'Angular Js',
+            'Vue Js',
+            'Node Js',
+            'Python',
+            'Express Js',
+            'Next Js',
+            'Nuxt Js',
+            'Flutter',
+            'Dart',
+            'Kotlin',
+        ]);
+        // $categories->each(function ($category) {
+        //     SubCategory::create([
+        //         'name' => $category,
+        //         'slug' => Str::slug($category)
+        //     ]);
+        // });
+
+        DB::table('sub_categories')->insert([
+            [
+                'name' => 'Php',
+                'slug' => 'php',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Java',
+                'slug' => 'java',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'JavaScript',
+                'slug' => 'javascript',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Html',
+                'slug' => 'html',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Laravel',
+                'slug' => 'laravel',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Code Igniter',
+                'slug' => 'code-igniter',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'React Js',
+                'slug' => 'react-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Angular Js',
+                'slug' => 'angular-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Vue Js',
+                'slug' => 'vue-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Node Js',
+                'slug' => 'node-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Express Js',
+                'slug' => 'express-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Next Js',
+                'slug' => 'next-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Nuxt Js',
+                'slug' => 'nuxt-js',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Python',
+                'slug' => 'python',
+                'category_id' => 1
+            ],
+            [
+                'name' => 'Flutter',
+                'slug' => 'flutter',
+                'category_id' => 2
+            ],
+            [
+                'name' => 'Dart',
+                'slug' => 'dart',
+                'category_id' => 2
+            ],
+            [
+                'name' => 'Kotlin',
+                'slug' => 'kotlin',
+                'category_id' => 2
+            ],
+            [
+                'name' => 'Figma',
+                'slug' => 'figma',
+                'category_id' => 2
+            ],
+            [
+                'name' => 'Go lang',
+                'slug' => 'go-lang',
+                'category_id' => 7
+            ],
+        ]);
     }
 }
