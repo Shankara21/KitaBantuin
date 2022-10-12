4d9137479cf5885571a22fdae3051ac6a3c98812
Firgisotya <zzuuddiinn28@gmail.com>
2022-10-12 10:30:18 +0700

    update 12-oct-2022

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.main');
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
