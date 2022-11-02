0b7154985fea4c803c4634ff5f1f9f35c741bd85
MahisaAghisni <aghisnifadhli@gmail.com>
2022-11-02 08:31:33 +0700

    About

--- resources/views/landingPage/about.blade.php
+++ resources/views/landingPage/about.blade.php
@@ -162,27 +162,12 @@
                 </div>
 
                 <div class="col-lg-5">
-                    <h2 class="section-title text-left mb-4">Take a look at Tour Video</h2>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
-                        the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
-                        language ocean.</p>
-
-                    <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary
-                        regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-                    </p>
-
-                    <ul class="list-unstyled two-col clearfix">
-                        <li>Outdoor recreation activities</li>
-                        <li>Airlines</li>
-                        <li>Car Rentals</li>
-                        <li>Cruise Lines</li>
-                        <li>Hotels</li>
-                        <li>Railways</li>
-                        <li>Travel Insurance</li>
-                        <li>Package Tours</li>
-                        <li>Insurance</li>
-                        <li>Guide Books</li>
-                    </ul>
+                    <h2 class="section-title text-left mb-4">Tentang Kita</h2>
+                    <p>Yang melatarbelakangi berdirinya usaha ini adalah adanya fakta bahawa sebagian besar masyarakat
+                        indonesia masih belum memiliki kemampuan dan pemahaman mengenai bidang IT. Bagi UMKM maupun pelaku
+                        usaha yang ingin mengembangkan usahanya, sering kali mengalami permasalahan berupa sulitnya
+                        pemasaran jika dilakukan secara konvensional, oleh sebab itu jasa ini sangat diperlukan untuk
+                        membantu perkembangan usaha tersebut</p>
 
                     <p><a href="#" class="btn btn-primary">Get Started</a></p>
 
